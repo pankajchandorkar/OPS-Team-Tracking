a8d2d3e8de690a8992122f264a81a05b4f6511c3
pankajchandorkar <pankaj.chandorkar@travelyaari.com>
2022-02-09 18:46:40 +0530

    development changes

--- Mobile-Ops-Team-Tracking/js/common.js
+++ Mobile-Ops-Team-Tracking/js/common.js
@@ -1,14 +1,22 @@
 $(document).ready(function () {
 
-    $("input[class='custome_radio']").on("click",function(){
-
-        $(".call-type-input").removeClass("active_custome_radio");
-
-        $(this).parent("div").addClass("active_custome_radio");
-
+    //for show slide-menu
+    $(".homepage-menu-icon").click(function () {
+        $('.slide-menu').toggle("slow");
     });
 
+    //for hide slide-menu
+    $(".close").click(function () {
+        $('.slide-menu').toggle("slow");
+    });
 
+    //for toggle active call type radio button
+    $("input[class='custome_radio']").on("click", function () {
+        $(".call-type-input").removeClass("active_custome_radio");
+        $(this).parent("div").addClass("active_custome_radio");
+    });
+
+    //for toggle tabs content
     $("#write-comment,#view-details").on("click", function () {
 
         var tabId = $(this).attr("id");
@@ -22,8 +30,39 @@ $(document).ready(function () {
         $(".page-tabs-content div[class='" + tabId + "']").show();
     });
 
+    //for back page
     $(".page-back-icon").on("click", function () {
         window.history.back();
     });
+
+    //for hide all list
+    function hideAllLists() {
+        $(".slide-list-header").hide();
+        $(".slidelist").hide();
+    }
+
+    //for operator name list
+    $("#txtOperatorName").on("click", function () {
+        hideAllLists();
+        $(".operatorNameSearch").show();
+        $("#txtsOperatorName").focus();
+        $(".slide-list-contain").addClass("show-slide-list");
+    });
+
+    $(".sl-back-icon").click(function () {
+        $(".slide-list-contain").removeClass("show-slide-list");
+    });
+
+    //for set drop down data
+    initDropDownData();
+
+    function initDropDownData(){
+
+        var opsData = {};
+        opsData.opName = [{}]
+
+
+        alert("test");
+    }
 
 });
